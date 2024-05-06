40d27d373625a6448e34b5e459deee71cf0921d3
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-06 15:19:18 +0200

    création de la classe ApprentiOrdonnateur

---
+++ saeBis/src/main/java/modele/ApprentiOrdonnateur.java
@@ -0,0 +1,33 @@
+package modele;
+
+import java.util.Collection;
+
+public class ApprentiOrdonnateur implements ConstantesCanvas {
+    private Position positionApprenti;
+
+    private Collection<Temple> temples;
+
+    private boolean crystalPorte;
+
+    public ApprentiOrdonnateur(){
+        // Position initiale du joueur
+        positionApprenti = new Position(LARGEUR_CANVAS/(CARRE*2),HAUTEUR_CANVAS/(CARRE*2) );
+    }
+
+
+    public Collection<Temple> getTemples() {
+        return temples;
+    }
+
+    public void setTemples(Collection<Temple> temples) {
+        this.temples = temples;
+    }
+
+    public Position getPosition() {
+        return positionApprenti;
+    }
+
+    public void setPositionApprenti(Position positionApprenti) {
+        this.positionApprenti = positionApprenti;
+    }
+}
