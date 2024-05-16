ce4cd2e09d6a01fc69c3e34e2a480c90e47b3054
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-16 14:55:52 +0200

    création de la classe cristal

---
+++ saeBis/src/main/java/modele/Cristal.java
@@ -0,0 +1,28 @@
+package modele;
+
+public class Cristal {
+    private Position positioncristal;
+
+    private int couleurcristal;
+
+    public Cristal(Position parPositioncristal, int parCouleurcristal ){
+        couleurcristal = parCouleurcristal;
+        positioncristal = parPositioncristal;
+    }
+
+
+
+
+    public int getCouleurCristal() {
+        return couleurcristal;
+    }
+
+    public Position getPositionCristal() {
+        return positioncristal;
+    }
+
+    public void setCouleurCristal(int couleurcristal) {
+        this.couleurcristal = couleurcristal;
+    }
+}
+
