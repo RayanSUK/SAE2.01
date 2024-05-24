fdcc1de957fb1a5c972d21457cdcf95a701ae54c
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-24 11:46:35 +0200

    création méthode échange cristaux

--- saeBis/src/main/java/modele/ApprentiOrdonnateur.java
+++ saeBis/src/main/java/modele/ApprentiOrdonnateur.java
@@ -1,49 +1,37 @@
 package modele;
 
-import javafx.geometry.Pos;
-
 import java.util.Collection;
 import java.util.List;
-
 public class ApprentiOrdonnateur implements ConstantesCanvas {
     private Position positionApprenti;
-
     private List<Temple> temples;
-
     private List<Cristal> cristaux;
-
-    private final Cristal [] cristalPorte = new Cristal[1];
-
+    private Cristal cristalPorte;
     public ApprentiOrdonnateur(){
-        // Position initiale du joueur
-        positionApprenti = new Position(LARGEUR_CANVAS/(CARRE*2),HAUTEUR_CANVAS/(CARRE*2) );
-
+ // Position initiale du joueur
+        positionApprenti = new Position(LARGEUR_CANVAS/(CARRE* 2),HAUTEUR_CANVAS/(CARRE*2) );
     }
-
 
     public Collection<Temple> getTemples() {
         return temples;
     }
-
     public void setTemples(List<Temple> temples) {
         this.temples = temples;
     }
 
     public Cristal echangeCristaux(Cristal cristal) {
-        if (positionApprenti.equals(cristal.getPositionCristal())) {
-            if (!(cristalPorte[0] == null)) {
-                Position positionCristal = cristalPorte[0].getPositionCristal();
-                int couleurCristal = cristal.getCouleurCristal();
-                Cristal cristal2 = new Cristal(positionCristal, couleurCristal);
-                cristalPorte[0] = cristal;
-                cristal = cristal2;
-            } else {
-                cristalPorte[0] = cristal;
+        if (!(cristalPorte == null)) {
+            Position positionCristal = cristalPorte.getPositionCristal();
+            int couleurCristal = cristal.getCouleurCristal();
+            Cristal cristal2 = new Cristal(positionCristal, couleurCristal);
+            cristalPorte = cristal;
+            cristal = cristal2;
 
-            }
-            return cristalPorte[0];
+        } else {
+            cristalPorte = cristal;
+
         }
-        return null;
+        return cristalPorte;
     }
 
     public Position getPosition() {
@@ -62,7 +50,7 @@ public class ApprentiOrdonnateur implements ConstantesCanvas {
         this.cristaux = cristaux;
     }
 
-    public Cristal[] getCristalPorte() {
+    public Cristal getCristalPorte() {
         return cristalPorte;
     }
 }
