0cddf68a25d437df4a772050020e6f778bf03af8
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-29 15:22:51 +0200

    modification méthode handle pour le bouton echanger cristaux

--- saeBis/src/main/java/controleur/Controleur.java
+++ saeBis/src/main/java/controleur/Controleur.java
@@ -15,25 +15,29 @@ import java.util.Collection;
 /** Le controleur fait le lien entre le modele et la vue*/
 public class Controleur implements EventHandler {
     public void handle(Event event) {
-        Object userData = ((MenuItem)event.getSource()).getUserData();
-        if (userData instanceof File) { //l'utilisateur a choisi un scénario
-            File fichierScenario = (File) userData;
-            System.out.println(fichierScenario.getName());
-            File scenario = fichierScenario;
-            List<Temple> temples = LectureScenario.lecture(fichierScenario).getKey();
-            List<Cristal> cristaux = LectureScenario.lecture(fichierScenario).getValue();
-            VBoxRoot.getCanvas().effacerCanva();
-            VBoxRoot.getApprenti().setTemples(temples);
-            VBoxRoot.getCanvas().setTemples(temples);
-            VBoxRoot.getApprenti().setCristaux(cristaux);
-            VBoxRoot.getCanvas().setCristaux(cristaux);
-            System.out.println(VBoxRoot.getApprenti());
-
-
-
-
+        if (event.getSource()instanceof MenuItem) {
+            Object userData = ((MenuItem)event.getSource()).getUserData();
+            if (userData instanceof File) { //l'utilisateur a choisi un scénario
+                File fichierScenario = (File) userData;
+                System.out.println(fichierScenario.getName());
+                File scenario = fichierScenario;
+                List<Temple> temples = LectureScenario.lecture(fichierScenario).getKey();
+                List<Cristal> cristaux = LectureScenario.lecture(fichierScenario).getValue();
+                VBoxRoot.getCanvas().effacerCanva();
+                VBoxRoot.getApprenti().setTemples(temples);
+                VBoxRoot.getCanvas().setTemples(temples);
+                VBoxRoot.getApprenti().setCristaux(cristaux);
+                VBoxRoot.getCanvas().setCristaux(cristaux);
+                System.out.println(VBoxRoot.getApprenti());
+            }
+        }
+        if (event.getSource()instanceof Button) {
+            if (((Button) event.getSource()).getText().equals("Echanger cristaux")) {
+                VBoxRoot.getCanvas().echangeGraphique();
+            }
         }
     }
+
 
 
 }
