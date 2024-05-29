6b439a64202d83908ceec3e4f677b6f238225205
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-29 22:57:20 +0200

    Modification de la JavaDoc

--- saeBis/src/main/java/modele/Temple.java
+++ saeBis/src/main/java/modele/Temple.java
@@ -1,25 +1,54 @@
 package modele;
 
+/**
+ * La classe temple représente les temples du jeu avec leurs caractéristiques
+ */
 public class Temple {
+    /**
+     * La position du temple, en final car elle est fixe
+     */
     private final Position positionTemple;
+
+    /**
+     * La couleur du temple
+     */
     private int couleurTemple;
 
 
+    /**
+     * Le constructeur de la classe qui reçoit en paramètre la position et la couleur du temple et les instancie
+     * @param parPosition, la position du temple
+     * @param parCouleurTemple, la couleur du temple
+     */
     public Temple(Position parPosition, int parCouleurTemple) {
         positionTemple = parPosition;
         couleurTemple = parCouleurTemple;
 
     }
 
+
     //création des accesseurs et des setteurs
+
+    /**
+     * Acceseur au champ temple
+     * @return couleurTemple, les couleurs des temples
+     */
     public int getCouleurTemple() {
         return couleurTemple;
     }
 
+    /**
+     * Modifie la couleur des temples
+     * @return couleurTemple, les couleurs des temples
+     */
     public void setCouleurTemple(int couleurTemple) {
         this.couleurTemple = couleurTemple;
     }
 
+    /**
+     * Accesseur au champ positionTemple
+     * @return positonTemple, la position du temple
+     */
     public Position getPositionTemple() {
         return positionTemple;
     }
