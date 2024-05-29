5d053602e6aeb920c7dd0b356d68e000f0e3e990
RayanSUK <sukkarie.rayan@gmail.com>
2024-05-29 22:57:13 +0200

    Modification de la JavaDoc

--- saeBis/src/main/java/modele/Position.java
+++ saeBis/src/main/java/modele/Position.java
@@ -1,20 +1,39 @@
 package modele;
 
+/**
+ * La classe position représente la position en abscisse et en ordonnée
+ */
 public class Position {
+    /**
+     * Compteur du nombre de pas
+     */
     private static int nombreDePas = 0;
+    /**
+     * La position en abscisse
+     */
     private int abscisse;
+
+    /**
+     * La position en ordonnée
+     */
     private int ordonnee;
+
+    /**
+     * Constructeur de la classe. Reçoit en paramètre la position abscisse et la position ordonnée puis les instancie
+     * @param abscisse, la position en abscisse
+     * @param ordonnee, la position en ordonnée
+     */
 
     public Position(int abscisse, int ordonnee){
         this.abscisse = abscisse;
         this.ordonnee = ordonnee;
     }
 
-    /*
-    La méthode deplacementUneCase déplace la position this d'une case
-    pour la rapprocher de celle du paramètre parPosition.
-    Elle incrémente le champ static nombreDePas
-    @param parPosition : la position vers laquelle this se rapproche
+    /**
+     * La méthode deplacementUneCase déplace la position this d'une case
+     * pour la rapprocher de celle du paramètre parPosition.
+     * Elle incrémente le champ static nombreDePas
+     * @param parPosition : la position vers laquelle this se rapproche
      */
 
     public void deplacementUneCase (Position parPosition){
@@ -40,6 +59,11 @@ public class Position {
 
     }
 
+    /**
+     * La méthode deplacementCristal déplace la position this d'un cristal
+     * pour la rapprocher de celle du paramètre parPosition.
+     * @param parPosition, la position vers laquelle this se rapproche
+     */
     public void deplacementCristal (Position parPosition){
         if (this.abscisse > parPosition.abscisse){
             this.abscisse -=1;
@@ -63,9 +87,11 @@ public class Position {
     }
 
 
-
-
-    //La méthode equals regarde si le joueur est arrivé à la position demandée.
+    /**
+     * Méthode qui reçoit en paramètre une position et regarde si les positions sont les mêmes et regarde si la position de l'objet appelant est la même.
+     * @param parPosition , la position demandée
+     * @return true si les positions sont les mêmes, sinon False
+     */
     public boolean equals(Position parPosition){
         if(this.ordonnee == parPosition.ordonnee){
             if(this.abscisse == parPosition.abscisse){
@@ -75,29 +101,50 @@ public class Position {
         return false;
     }
 
-    //Les gets
+    /**
+     * Accesseur au champ nombreDePas
+     * @return nombreDePas, le nombre de pas du joueur
+     */
     public static int getNombreDePas(){
         return nombreDePas;
     }
 
+    /**
+     * Accesseur au champ abscisse
+     * @return abscisse, la position en abscisse
+     */
     public int getAbscisse(){
         return abscisse;
     }
+
+    /**
+     * Accesseur au champ ordonnee
+     * @return ordonnee, la position en ordonnée
+     */
 
     public int getOrdonnee(){
         return ordonnee;
     }
 
-    public String toString(){
-        return("Position : " + (this.getAbscisse()-16) + ", " + (this.getOrdonnee()-16));
-    }
-
+    /**
+     * Reçoit en paramètre parAbscisse et parOrd pour modifier la position
+     * @param parAbscisse, position en abscisse
+     * @param parOrd , position en ordonnée
+     */
     public void setPosition(int parAbscisse, int parOrd){
         abscisse = parAbscisse;
         ordonnee = parOrd;
     }
 
+    /**
+     * Retourne sous forme de texte la position avec son abscisse et son ordonnée
+     * @return une chaine de caractère
+     */
+    public String toString(){
+        return("Position : " + (this.getAbscisse()-16) + ", " + (this.getOrdonnee()-16));
+    }
 
-    //La méthode toString
+
+
 
 }
