f1390cca750c0f9ede2fceb9c65fc41a5d455afc
RayanSUK <sukkarie.rayan@gmail.com>
2024-06-05 03:47:16 +0200

    ajout documentation

--- saeBis/src/main/java/vue/HBoxRoot.java
+++ saeBis/src/main/java/vue/HBoxRoot.java
@@ -2,10 +2,16 @@ package vue;
 
 import javafx.scene.layout.HBox;
 
+/**
+ * La classe HBoxRoot hérite de HBox et organise les 2 VBox (VBoxGauche et VBoxDroite) horizontalement.
+ */
 public class HBoxRoot extends HBox {
     private VBoxGauche VBoxGauche;
     private VBoxDroite VBoxDroite;
 
+    /**
+     * Constructeur de la classe. Instancie les deux boites et les ajoute à la HBox.
+     */
     public HBoxRoot(){
         VBoxGauche = new VBoxGauche();
         VBoxDroite = new VBoxDroite();
