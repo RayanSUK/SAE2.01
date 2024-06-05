7245fc21f1e6f19bfbbc5bfbc138f18755ab1c2a
RayanSUK <sukkarie.rayan@gmail.com>
2024-06-05 03:56:17 +0200

    ajustement

--- saeBis/src/main/java/modele/LectureScenario.java
+++ saeBis/src/main/java/modele/LectureScenario.java
@@ -5,7 +5,6 @@ import javafx.util.Pair;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,13 +12,13 @@ import java.util.Scanner;
  * La classe LectureScenario permet de lire les scénarios et de créer la liste des temples et des cristaux
  */
 public class LectureScenario implements ConstantesCanvas{
+
     /**
      * La méthode de lecture prend en paramètre un fichier contenant un scénario
-     * et retourne une pair de donnée contenant les temples à réaligner ainsi que les cristaux
+     * et retourne une paire de donnée contenant les temples à réaligner ainsi que les cristaux
      * @param fichierScenario : le fichier lu
      * @return une paire de list de cristaux et de cristaux
      */
-
     public static Pair<List<Temple>, List<Cristal>> lecture(File fichierScenario){
         List <Temple> templesDuScenario = new ArrayList<>();
         List <Cristal> cristaux = new ArrayList<>();
@@ -27,7 +26,7 @@ public class LectureScenario implements ConstantesCanvas{
             Scanner scanner = new Scanner(fichierScenario);
             while (scanner.hasNext()){
                 //Largeur_CANVAS = 31 HAUTEUR_CANVAS = 31
-                // permet de traiter tout les scénarios poposés
+                // permet de traiter tous les scénarios proposés
 
                 int posX = scanner.nextInt() + LARGEUR_CANVAS/(2*CARRE);
 
@@ -48,6 +47,6 @@ public class LectureScenario implements ConstantesCanvas{
         catch (FileNotFoundException e){
             System.out.println(e.getMessage());
         }
-        return new Pair<List<Temple>, List<Cristal>>((List<Temple>) templesDuScenario, cristaux);
+        return new Pair<List<Temple>, List<Cristal>>(templesDuScenario, cristaux);
     }
 }
