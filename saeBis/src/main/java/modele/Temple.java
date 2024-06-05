c556d2aac27a104fcccec5783aec3b7fc245c571
RayanSUK <sukkarie.rayan@gmail.com>
2024-06-05 03:48:01 +0200

    ajout de la documentation

--- saeBis/src/main/java/modele/Temple.java
+++ saeBis/src/main/java/modele/Temple.java
@@ -14,7 +14,6 @@ public class Temple {
      */
     private int couleurTemple;
 
-
     /**
      * Le constructeur de la classe qui reçoit en paramètre la position et la couleur du temple et les instancie
      * @param parPosition, la position du temple
@@ -26,8 +25,7 @@ public class Temple {
 
     }
 
-
-    //création des accesseurs et des setteurs
+    //création des accesseurs et des setters
 
     /**
      * Acceseur au champ temple
@@ -53,6 +51,10 @@ public class Temple {
         return positionTemple;
     }
 
+    /**
+     * Retourne sous forme de texte la position du temple ainsi que sa couleur
+     * @return une chaine de caractère
+     */
     public String toString() {
         return positionTemple.toString() + " " + couleurTemple;
     }
