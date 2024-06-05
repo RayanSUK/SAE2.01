b656fd0058e0b41d0da7077bc36e2a241f35f512
RayanSUK <sukkarie.rayan@gmail.com>
2024-06-05 03:47:05 +0200

    ajout documentation , ajout du bouton réinitialiser

--- saeBis/src/main/java/vue/HBoxBouton.java
+++ saeBis/src/main/java/vue/HBoxBouton.java
@@ -6,9 +6,9 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-
-
-
+/**
+ * La classe HBoxBouton permet de créer un HBox contenant deux boutons et un label
+ */
 public class HBoxBouton extends HBox {
 
     /**
@@ -22,11 +22,24 @@ public class HBoxBouton extends HBox {
     private Button boutonDeposer;
 
     /**
+     * Bouton pour recharger/réinitialisé le jeu
+     */
+    private Button boutonRestart;
+
+    /**
      * Label lorsque le joueur gagne
      */
     private Label labelVictoire;
 
+    /**
+     * Constructeur de la classe.
+     * Instancie les 3 boutons et les mets sous écoute pour pouvoir communiquer avec le contrôleur
+     * Les boutons sont associés à l'EventHandler, qui gère les événements de clic
+     * @param eventHandler le event Handler
+     */
     public HBoxBouton(EventHandler<ActionEvent> eventHandler) {
+
+        // le bouton pour échanger les cristaux
         boutonEchanger = new Button("Échanger les cristaux");
         boutonEchanger.setOnAction(eventHandler);
         this.getChildren().add(boutonEchanger);
@@ -35,6 +48,11 @@ public class HBoxBouton extends HBox {
         boutonDeposer = new Button("Déposer cristal");
         boutonDeposer.setOnAction(eventHandler);
         this.getChildren().add(boutonDeposer);
+
+        // le bouton pour réinitialiser
+        boutonRestart = new Button("Réinitialiser");
+        boutonRestart.setOnAction(eventHandler);
+        this.getChildren().add(boutonRestart);
 
         //Le label de victoire
         labelVictoire = new Label("Victoire");
@@ -45,17 +63,34 @@ public class HBoxBouton extends HBox {
 
     }
 
+    /**
+     * Méthode qui vérifie si la partie est gagnée pour afficher le label victoire.
+     */
     public void conditionVictoire(){
         labelVictoire.setVisible(VBoxGauche.getApprenti().victoire());
     }
 
+    /**
+     * Accesseur au champ
+     * @return boutonDéposer
+     */
     public Button getBoutonDeposer() {
         return boutonDeposer;
     }
 
+    /**
+     * Accesseur au champ
+     * @return boutonEchanger
+     */
+
     public Button getBoutonEchanger() {
         return boutonEchanger;
     }
+
+    /**
+     * Accesseur au champ
+     * @return labelVictoire
+     */
 
     public Label getLabelVictoire() {
         return labelVictoire;
